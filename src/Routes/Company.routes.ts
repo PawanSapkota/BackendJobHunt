1a1cb9c39ce8b8d1e7323f3a712ff032314e3c16
Pawan Sapkota <sapkotapawan33@gmail.com>
2023-10-06 20:52:03 +0545

    image added

--- src/Routes/Company.routes.ts
+++ src/Routes/Company.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { companyPostHandler } from "../controller/Company.controller";
+import { upload } from "../Utils/UploadFile";
 const router = Router();
 
-router.route("/").post(companyPostHandler);
+router.route("/").post(upload.single("image"), companyPostHandler);
 
 export default router;
